82636d8e8f9cc900cc676297a07dea53623ed9e6
hernanveyret <hernanveyret@hotmail.com>
2024-08-01 12:29:47 -0300

    clase 7

--- promesas.js
+++ promesas.js
@@ -1,1 +1,32 @@
-const miPromesa = new Promise((resolve, reject));
+const promesaCumplida = false;
+
+const miPromesa = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if(promesaCumplida){
+      resolve('Promesa cumplida');
+    }else{
+      reject('Promesa rechazada...')
+    }
+  },3000);
+});
+/* opcion uno para mostrar que pasa si la proimesa es exito o rechazada
+const manejarExito = ( valor ) => {
+  console.log(valor);
+}
+const manejarRechazo = ( razonRechazo ) => {
+  console.log(razonRechazo);
+}
+
+miPromesa.then(manejarExito, manejarRechazo);
+*/
+
+/*
+opcion 2, usando .then().catch().finaly()
+*/
+miPromesa.then((valor) => {
+  console.log(valor);
+}).catch((error) => {
+  console.log(error);
+}).finally(() => {
+  console.log('Que tengas un buen dia!!!')
+});
